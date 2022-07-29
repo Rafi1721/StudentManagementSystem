e7c8bfb3de656c1caddca3b19fd84d83bcd79bd3
Syed Rafi <syed.rafi@indiumsoft.com>
2022-07-29 10:59:16 +0530

    Validation is done

--- StudentManagementSystem/Model/User.cs
+++ StudentManagementSystem/Model/User.cs
@@ -1,8 +1,13 @@
-﻿namespace StudentManagementSystem.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentManagementSystem.Model
 {
     public class User
     {
         public string UserName { get; set; } = string.Empty;
+
+        [Required]
+        [RegularExpression("^(?=.*_[A-Za-z])(?=.*_)[A-Za-z]{8,15}$")]
         public string Password { get; set; } = string.Empty;
     }
 }
